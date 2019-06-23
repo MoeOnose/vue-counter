1b7177972dcb9a9461dacef71893bf4c4e65f4f5
Moe Onose <onose.moe.infratop@gmail.com>
2019-06-24 07:52:11 +0900

    やったー🐣

---
+++ src/components/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <!-- <p>Hello, World!</p> -->
+    <counter/>
+  </div>
+</template>
+
+<script>
+import counter from './Counter' 
+export default {
+    components: {counter}    
+}
+</script>
+
+
+<style scoped>
+  * {
+    border: 10px solid blue;
+  }
+</style>
---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+const CopyPlugin = require('copy-webpack-plugin')
+const path = require('path')
+
+module.exports = {
+    //root
+    entry: './src/index.js',
+    //出力先
+    output: {
+        path: path.resolve(__dirname, './dest'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+          {
+            test: /\.vue$/, // ファイルが.vueで終われば...
+            loader: 'vue-loader' // vue-loaderを使う
+          },
+          {
+            test: /\.js$/,
+            loader: 'babel-loader',
+          },
+          {
+            test: /\.css$/,
+            use: ['vue-style-loader', 'css-loader'] // css-loader -> vue-style-loaderの順で通していく
+          },
+        ]
+      },
+      resolve: {
+        // import './foo.vue' の代わりに import './foo' と書けるようになる(拡張子省略)
+        extensions: ['.js', '.vue'],
+        alias: {
+          // vue-template-compilerに読ませてコンパイルするために必要
+          vue$: 'vue/dist/vue.esm.js',
+        },
+      },
+    plugins: [
+        new VueLoaderPlugin(),
+        new CopyPlugin([{ from: './public' }])
+    ],
+    devServer: {
+        // webpackの扱わないファイル(HTMLや画像など)が入っているディレクトリ
+        //TODO: staticに変更
+        contentBase: path.resolve(__dirname, 'pablic')
+    }
+}
